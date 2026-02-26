5be2ee38bf5cfdc82780cc5712164cc105c9511d
Cindy Hech <archerychinadiva@gmail.com>
2026-02-26 01:33:48 -0500

    Reusable components

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import Todo from './components/Todo.jsx'
+import Title from './components/Title.jsx'
+
+function App() {
+  return (
+    <div>
+      <Title></Title>
+      <div className="todo__wrapper">
+        <Todo></Todo>
+        <Todo/>
+        <Todo/>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ components/Title.jsx
@@ -0,0 +1,9 @@
+function Title() {
+    return (
+    <div>
+     <h1>My To-do List</h1>
+    </div>     
+ ); 
+}
+
+export default Title;
