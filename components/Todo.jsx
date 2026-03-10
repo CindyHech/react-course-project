6407736b930bbc546c31d6c8d163a05372444c06
Cindy Hech <archerychinadiva@gmail.com>
2026-03-10 01:56:28 -0400

    Props added

--- components/Todo.jsx
+++ components/Todo.jsx
@@ -1,12 +1,17 @@
 import './Todo.css'
 
-function Todo() {
+function Todo({title, paragraph}) {
+  console.log(title)  
   return (
     <div className='todo'> 
-      <h2>Finish Frontend Simplified</h2>
-      <button>Delete</button>
+    <h2>{title}</h2>
+    <p>{paragraph}</p>
+    <button>Delete</button>
     </div>
   );
 }
 
+
 export default Todo;
+
+
