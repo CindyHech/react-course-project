f94f223c073c3252a9e3573d51dbcd2fbf79cb90
Cindy Hech <archerychinadiva@gmail.com>
2026-05-20 22:12:37 -0400

    Implemented react hookuse effect and state

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import "./App.css";
+import Todo from "./components/Todo.jsx";
+import Title from "./components/Title.jsx";
+import Modal from "./components/Modal.jsx";
+import Counter from "./components/Counter.jsx";
+import React, { useState, useEffect } from "react";
+
+
+function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  function onTodoDelete() {
+    setShowModal(true);
+    console.log("onTodoDelete()");
+  }
+
+  function cancelModal() {
+    setShowModal(false);
+  }
+
+  function confirmModal() {
+    setShowModal(false);
+  }
+
+  useEffect(() => {
+    console.log('on mount')
+  }, [])
+
+  return (
+    <div>
+      <Title />
+      <div>
+        <input
+          type="text"
+          onChange={(event) => {
+            console.log(event.target.value);
+          }}
+        />
+        <button onClick={() => setShowModal(true)}>Add todo</button>
+      </div>
+      <div className="todo__wrapper">
+        <Todo onTodoDelete={onTodoDelete} title="Finish Frontend Simplified" />
+        <Todo onTodoDelete={onTodoDelete} title="Finish Interview Section" />
+        <Todo onTodoDelete={onTodoDelete} title="Land a $100K Job" />
+      </div>
+      {showModal && (
+        <Modal
+          cancelModal={cancelModal}
+          confirmModal={confirmModal}
+          title="Confirm Delete?"
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
